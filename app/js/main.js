3f1c6943ff195d1d73a1cc7f9f036ac0d6ac3443
IAndresI <andrei200277@gmail.com>
2020-07-11 02:40:54 +0300

    add slider-info

--- app/js/main.js
+++ app/js/main.js
@@ -7,4 +7,20 @@ document.addEventListener("DOMContentLoaded", function () {
   slider(".slider__item", ".slider__translatex", ".products-slider__arrows-next", ".products-slider__arrows-prev");
   rate(".rate-star");
   tabs();
+
+  let slider_image = document.querySelectorAll(".item__image-container"),
+    more_info = document.querySelectorAll(".item__hover"),
+    image = document.querySelectorAll(".item__image-newest");
+
+  for (let i = 0; i < slider_image.length; i++) {
+    slider_image[i].addEventListener('mouseenter', function () {
+      more_info[i].style.width = "100%";
+      image[i].style.width = "0";
+    });
+
+    slider_image[i].addEventListener('mouseleave', function () {
+      more_info[i].style.width = "0";
+      image[i].style.width = "100%";
+    });
+  }
 });
