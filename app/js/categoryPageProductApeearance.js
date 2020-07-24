d3eca7d3b9d61a5eed0be3cc94d1cb47ca5b9417
IAndresI <andrei200277@gmail.com>
2020-07-24 23:21:07 +0300

    fix filter icons

--- app/js/categoryPageProductApeearance.js
+++ app/js/categoryPageProductApeearance.js
@@ -1,19 +1,26 @@
 function productAppearance() {
   let radio_grid = document.querySelector(".filter__radio--grid"),
-    radio_list = document.querySelector(".filter__radio--list");
+    radio_list = document.querySelector(".filter__radio--list"),
+    radio_grid_svg = document.querySelector(".filter__image--grid svg path"),
+    radio_list_svg = document.querySelector(".filter__image--list svg path");
 
+  radio_grid_svg.classList.add("asd");
   radio_grid.addEventListener("change", function () {
-    let items_grid = document.querySelectorAll(".product-item--category");
+    let items_grid = document.querySelectorAll(".product-item--category-lg");
+    radio_grid_svg.classList.add("asd");
+    radio_list_svg.classList.remove("asd");
     items_grid.forEach(element => {
-      element.classList.add("product-item--category-lg");
+      element.classList.remove("product-item--category-lg");
+      element.classList.add("product-item--category");
     });
   });
 
   radio_list.addEventListener("change", function () {
-    let items_list = document.querySelectorAll(".product-item--category-lg");
+    let items_list = document.querySelectorAll(".product-item--category");
+    radio_list_svg.classList.add("asd");
+    radio_grid_svg.classList.remove("asd");
     items_list.forEach(element => {
-      element.classList.remove("product-item--category-lg");
-      element.classList.add("product-item--category");
+      element.classList.add("product-item--category-lg");
     });
   });
 }
--- app/js/main.js
+++ app/js/main.js
@@ -24,6 +24,7 @@ document.addEventListener("DOMContentLoaded", function () {
     });
   }
 
+
   navMiniDropdown();
   rangeSlider();
   rate(".rate-star");
